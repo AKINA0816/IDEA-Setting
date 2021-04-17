28cc0e685f71cca4b874b121ffcdcedbcce47b52
Chen <kevinyang9708@gmail.com>
2021-04-17 11:38:08 -0400

    IU-2021.1 <Chen Yang@DESKTOP-ULRE49R Create ide.general.xml, ide-features-trainer.xml, project.default.xml, laf.xml, Class.java, jdk.table.xml, shared-indexes.xml, AMyTemplates.xml, path.macros.xml, visualizationTool.xml, ui.lnf.xml, wsl.distributions.xml, filetypes.xml, vcs.xml, colors.scheme.xml, grazie_global.xml, databaseDrivers.xml, editor.xml, github.xml, templates.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @program: ${NAME}
+ * @description: Todo
+ * @author: ${USER}
+ * @create: ${DATE} ${TIME}
+ * @version: 1.0  
+ **/
+public class ${NAME} {
+}
